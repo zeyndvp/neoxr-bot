12628bd94b050da2815703c42a0527ee6e20402f
Zeyn <118685103+zeyndvp@users.noreply.github.com>
2025-01-19 20:03:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const env = require('./config.json')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 7860
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 let i = 0
 
